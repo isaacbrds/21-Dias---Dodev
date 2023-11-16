2f2a12e533dbce35ed8b94c51daef7cdb59063f7
isaac <isaac.brigido@gmail.com>
2023-11-16 10:15:11 -0300

    Dia 4 - Aula 1

---
+++ Dia 4/index.js
@@ -0,0 +1,9 @@
+let idade = Number(prompt('Insira sua idade'))
+
+
+if (idade >= 60){
+    console.log('Você está na terceira idade pegue sua carteirinha de idoso!')
+}else{
+    console.log('Você ainda não possui idade para carteirinha do idoso :(')
+}
+
