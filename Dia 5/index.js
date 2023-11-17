1a52ab621a7060d9eb22e622181bc00fb0cd52fb
isaac <isaac.brigido@gmail.com>
2023-11-17 10:22:01 -0300

    Dia 5 - Aula 1

---
+++ Dia 5/index.js
@@ -0,0 +1,14 @@
+let idade = Number(prompt('Digite sua Idade'))
+
+
+switch(idade){
+    case 13:
+        console.log('Voce tem 15 anos')
+        break;
+    case 15:
+        console.log('Voce tem 15 anos')
+        break;
+    default:
+        console.log('Não sei sua idade cara!')
+        break;
+}
