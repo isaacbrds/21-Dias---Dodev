f8ab631d53b0479fa2bc31a4c9d97ef86f0acf93
isaac <isaac.brigido@gmail.com>
2023-11-22 11:15:48 -0300

    Dia 10 - Exercicio 02

--- Dia 10/index.js
+++ Dia 10/index.js
@@ -1,7 +1,8 @@
 let array = []
 
-for(let contador = 0; contador < 11; contador++){
-    array[contador] = contador
+for(let contador = 5; contador > 0; contador--){
+    let numero = prompt('Digite o valor do numero')
+    array[contador] = numero;
 }
 
 console.log(array)
