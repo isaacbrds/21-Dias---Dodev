3bd0b9b093cfa970f5f6d894d3e8642dc151c90c
isaac <isaac.brigido@gmail.com>
2023-11-22 11:33:07 -0300

    Dia 10 - Exercicio 03

--- Dia 10/index.js
+++ Dia 10/index.js
@@ -1,8 +1,11 @@
-let array = []
+let quantidadeDeNumeros = Number(prompt('Digite a quantidade de números que deseja inserir'))
+let array = [];
+let arrayContrario = [];
 
-for(let contador = 5; contador > 0; contador--){
-    let numero = prompt('Digite o valor do numero')
+for(let contador = 0; contador < quantidadeDeNumeros ; contador++){
+    let numero = Number(prompt('Digite o valor do numero'));
     array[contador] = numero;
+    arrayContrario[quantidadeDeNumeros - contador - 1] = numero;
 }
 
-console.log(array)
+console.log(array, arrayContrario)
